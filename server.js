e6ff457596ef6dd870baefaeacc0837636cc4979
Aditya krishna vamsy Mudragada <46970931+adityakrishnavamsy@users.noreply.github.com>
2022-10-23 10:33:13 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,64 @@
-hi
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io')
+const path = require('path')
+const app = express();
+const server = http.createServer(app);
+const formatMessage = require('./utils/messages')
+const io = socketio(server);
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+// setting a static folder to view contents on frontned 
+app.use(express.static(path.join(__dirname, 'public')))
+
+const botName = 'Chat bot'
+
+// run when client connects 
+io.on('connection', socket => {
+    console.log('new WS connection..');
+
+    socket.on('joinRoom', ({ username, room }) => {
+
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+        // Welcome the current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+        // boardcast the message to everyone except the user
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `A ${user.username} has joined the chat`));
+        // send users and room info 
+        io.to(user.room).emit('roomUser', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+
+
+
+    // listen for the chat message from client and emit it back to the client 
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+
+    });
+    // Runs when the client disconnects 
+    socket.on("disconnect", () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit(
+                "message",
+                formatMessage(botName, `${user.username} has left the chat`)
+            );
+
+            // Send users and room info
+            io.to(user.room).emit("roomUsers", {
+                room: user.room,
+                users: getRoomUsers(user.room),
+            });
+        }
+    });
+});
+
+
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ utils/users.js
@@ -0,0 +1,38 @@
+// as of now we are storing the user's related info in memory but if needed 
+// we can use a database 
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+};
